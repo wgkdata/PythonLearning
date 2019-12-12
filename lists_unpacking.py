3f2c8669caace0483612e3696728b49ce1aff227
wgkdata <luan.lps01@gmail.com>
2019-12-12 12:06:56 -0300

    last add

---
+++ lists_unpacking.py
@@ -0,0 +1,5 @@
+lista = ['Luiz', 'João', 'Maria', 'Lisa', 'Gabriel', 'Pedro']
+
+n1, n2, *_, last = lista
+
+print(_, last)
---
+++ split_join_enum.py
@@ -0,0 +1,35 @@
+'''
+    * Split - Divide uma string # str
+    * Join - Junta uma lista # str
+    * Enumerate - Enumerar elem da lista # list/iterable
+    * Strip - Remove espaços do início e fim # str
+'''
+
+string = "STORROR was here and did jumps, Rio has a huge culture."
+
+list1 = string.split(' ')
+
+'''
+list2 = string.split(',')
+
+#print(list)
+
+palavra = ''
+contagem = 0
+
+for word in list1:
+    word_qtd = list1.count(word)
+
+    if word_qtd > contagem:     # Checa a palavra que apareceu mais vezes;
+        contagem = word_qtd
+        palavra = word
+
+print(f'\'{palavra}\' was here a total of {contagem} times!')
+'''
+
+string_join = '_'.join(list1)
+
+#print(f'{string} \n{list1} \n{string_join}')
+
+for indice, valor in enumerate(list1):
+    print(indice, valor)
