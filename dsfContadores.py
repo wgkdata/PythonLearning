71d79a1597595e35af1cfdd9d7cd7c382ef5bc00
wgkdata <luan.lopes@catolicasc.org.br>
2020-10-08 17:00:19 -0300

    ''

--- dsfContadores.py
+++
@@ -1,5 +0,0 @@
-cpf = '124.336.779-20'
-
-work_with = cpf.split('.')
-
-print(work_with)
--- funcoes_3.py
+++
@@ -1,7 +0,0 @@
-def func(*args, **kwargs):
-#    print(args, len(args))
-#    print(kwargs['surname'])
-    nome = kwargs.get('nome')
-lista = [1,2,3,4,5,6]
-
-func(*lista, name='Krishna', surname='Raj')
